7e8fa59a86ee6ae5aa61f919ded9b19642cc54dd
colombefioren <colomberakotonjanahary@gmail.com>
2025-03-02 17:47:26 +0300

    [ADD] Added image with transition effect

--- app/Movie/[id].tsx
+++ app/Movie/[id].tsx
@@ -1,11 +1,41 @@
+import { fruitItems } from "@/components/TrendingCarousel";
 import { useLocalSearchParams } from "expo-router";
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground, Dimensions, StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
 const MovieScreen = () => {
   const { id } = useLocalSearchParams();
+  const { width, height } = Dimensions.get("window");
+  const item = fruitItems.filter((film) => film.id === +id)[0];
+
   return (
     <View>
-      <Text>{id} Movie!!!</Text>
+      <ImageBackground
+        source={{ uri: item.image }}
+        style={[styles.image, { height: height * 0.45 }]}
+        resizeMode="cover"
+      >
+        <LinearGradient
+          colors={["transparent", "rgba(27, 36, 49, 0.6)", "#1B2431"]}
+          locations={[0, 0.3, 1]}
+          style={styles.gradient}
+        />
+      </ImageBackground>
+
+      <View>
+        <Text>{item.title}</Text>
+      </View>
     </View>
   );
 };
+
 export default MovieScreen;
+
+const styles = StyleSheet.create({
+  gradient: {
+    height: "100%",
+    width: "100%",
+    position: "absolute",
+    bottom: 0,
+  }
+});
