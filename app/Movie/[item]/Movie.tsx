35164ccabefe62b9087bb5db3715d91dded0051b
coco <cocobrowniees@gmail.com>
2025-03-02 15:21:08 +0300

    Initial commit

--- app/Movie/[item]/Movie.tsx
+++ app/Movie/[item]/Movie.tsx
@@ -2,10 +2,9 @@ import { useLocalSearchParams } from "expo-router";
 import { View, Text } from "react-native";
 const Movie = () => {
 
-  // const { item } = useLocalSearchParams;
   return (
     <View>
-      <Text></Text>
+      <Text>Hello</Text>
     </View>
   );
 };
