bfcd19b17c25d39725f0bb450a0ea3982531bde0
colombefioren <colomberakotonjanahary@gmail.com>
2025-03-13 17:03:25 +0300

    [ADD] Added the cast screen

--- components/Cast.tsx
+++ components/Cast.tsx
@@ -1,11 +1,15 @@
 import { View, Text, TouchableOpacity, Image, Dimensions } from "react-native";
 import Carousel from "react-native-reanimated-carousel";
 import { fruitItems } from "./TrendingCarousel";
+import { router } from "expo-router";
 const Cast = () => {
   const { width, height } = Dimensions.get("window");
   return (
     <View className="flex gap-5">
-      <Text className="font-lexendSemi text-white mt-5 text-lg" style={{marginLeft : 28}}>
+      <Text
+        className="font-lexendSemi text-white mt-5 text-lg"
+        style={{ marginLeft: 28 }}
+      >
         Top Cast
       </Text>
       <Carousel
@@ -14,7 +18,7 @@ const Cast = () => {
         width={width * 0.95}
         height={height * 0.16}
         renderItem={({ item }) => (
-          <TouchableOpacity>
+          <TouchableOpacity onPress={() => router.push("/Cast/[id]")}>
             <View
               style={{ width: 75 }}
               className="flex items-center ml-5 gap-3 "
