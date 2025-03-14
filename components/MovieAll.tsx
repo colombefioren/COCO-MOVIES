4a05a56be7cc336c3c0a8dc6ca374ef73ed7dce5
colombefioren <colomberakotonjanahary@gmail.com>
2025-03-14 19:31:32 +0300

    [FEAT] Added a default pic in the search

--- components/MovieAll.tsx
+++ components/MovieAll.tsx
@@ -1,4 +1,11 @@
-import { View, FlatList, Dimensions, Pressable, Text } from "react-native";
+import {
+  View,
+  FlatList,
+  Dimensions,
+  Pressable,
+  Text,
+  Image,
+} from "react-native";
 import { Items } from "./TrendingCarousel";
 import MovieCard from "./MovieCard";
 import { router } from "expo-router";
@@ -8,11 +15,23 @@ const MovieAll = ({ data, search }: { data: Items[]; search: boolean }) => {
 
   return (
     <>
-      {search && (
+      {search && data.length > 0 && (
         <Text className="text-lg ml-5 mb-4">
           <Text className="font-lexendSemi text-secondary">Results</Text>
           <Text className="font-lexendRegular text-white">{` (${data.length})`}</Text>
         </Text>
+      )}
+      {search && data.length === 0 && (
+        <Image
+          style={{
+            width: width * 0.83,
+            height: height * 0.3,
+            objectFit: "cover",
+            marginInline: "auto",
+            marginTop: 60,
+          }}
+          source={require("../assets/images/searchImage.png")}
+        />
       )}
       <FlatList
         contentContainerStyle={{
