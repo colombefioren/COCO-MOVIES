52357f0ed6ed5e15401db83756450b566fce224c
colombefioren <colomberakotonjanahary@gmail.com>
2025-05-04 17:04:56 +0300

    update: fixed dynamic data in movie screen

--- app/Movie/[id].tsx
+++ app/Movie/[id].tsx
@@ -1,4 +1,3 @@
-import { fruitItems } from "@/components/TrendingCarousel";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import {
   View,
@@ -17,20 +16,26 @@ import { transformTime } from "@/components/MovieCard";
 import { useEffect, useState } from "react";
 import Cast from "@/components/Cast";
 import MovieList from "@/components/MovieList";
+import { fetchMovieByID, image500 } from "@/services/tmdb";
+import { MovieItem } from "@/types/movieItem";
 
 const MovieScreen = () => {
   const [long, setLong] = useState(false);
   const { id } = useLocalSearchParams();
   const { height } = Dimensions.get("window");
-  const item = fruitItems.filter((film) => film.id === +id)[0];
   const router = useRouter();
   const [desc, setDesc] = useState("");
   const ios = Platform.OS == "ios";
   const iconSize = ios ? 52 : 45;
   const { heure, minute } = transformTime("201");
   const [show, setShow] = useState(false);
-  let description =
-    "Snow White is not set typically means That’s rough. You’re basically carrying them, and they probably don’t even realize how frustrating it is for you. Maybe it’s time to start pushing back a little—like asking them what they’ve tried before giving an answer. If they haven’t even made an effort, they don’t deserve an easy solution.";
+  const [item, setItem] = useState<MovieItem>();
+
+  const getCurrentItem = async (id: number) => {
+    const currentMovieData = await fetchMovieByID(id);
+    setItem(currentMovieData);
+    setDesc(currentMovieData?.overview);
+  };
 
   const descLength = (str: string) => {
     if (str.length > 150) {
@@ -44,17 +49,17 @@ const MovieScreen = () => {
   };
 
   useEffect(() => {
-    descLength(description);
+    getCurrentItem(+id);
   }, []);
 
   const handleMoreClick = () => {
-    setDesc(description);
+    setDesc(desc);
     setLong(false);
     setShow(true);
   };
 
   const handleShowClick = () => {
-    setDesc(description.slice(0, 150) + "...");
+    setDesc(desc.slice(0, 150) + "...");
     setLong(true);
     setShow(false);
   };
@@ -62,132 +67,142 @@ const MovieScreen = () => {
   return (
     <ScrollView>
       <StatusBar style="light" backgroundColor="transparent" />
-      <ImageBackground
-        source={{ uri: item.image }}
-        style={{ height: height * 0.48, backgroundColor: "red" }}
-        resizeMode="cover"
-      >
-        <LinearGradient
-          colors={["transparent", "rgba(27, 36, 49, 0.6)", "#1B2431"]}
-          locations={[0, 0.6, 1]}
-          style={styles.gradient}
-        />
+      {item && (
+        <>
+          <ImageBackground
+            source={{
+              uri:
+                image500(item.poster_path) ??
+                "https://theredwindows.net/wp-content/themes/koji/assets/images/default-fallback-image.png",
+            }}
+            style={{ height: height * 0.48, backgroundColor: "red" }}
+            resizeMode="cover"
+          >
+            <LinearGradient
+              colors={["transparent", "rgba(27, 36, 49, 0.6)", "#1B2431"]}
+              locations={[0, 0.6, 1]}
+              style={styles.gradient}
+            />
 
-        <TouchableOpacity className="absolute  top-[75%] left-[50%] translate-x-[-50%]">
-          <View
-            style={{ height: iconSize, width: iconSize }}
-            className="rounded-full bg-secondaryTrans flex items-center justify-center"
-          >
-            <FontAwesome
-              name="play"
-              size={ios ? 28 : 25}
-              color={"#0688F3"}
-              className="ml-2"
-            />
-          </View>
-        </TouchableOpacity>
-        <View
-          className="flex flex-row justify-between px-5"
-          style={ios ? { marginTop: 60 } : { marginTop: 30 }}
-        >
-          <TouchableOpacity onPress={() => router.push("/")}>
-            <FontAwesome name="chevron-left" color={"white"} size={25} />
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <FontAwesome name="tv" color={"white"} size={25} />
-          </TouchableOpacity>
-        </View>
-      </ImageBackground>
-      <View className="flex items-center justify-center px-10 w-full mt-[-25px]">
-        <Text className="text-white font-lexendSemi text-center text-xl">
-          {item.title}
-        </Text>
-      </View>
-      <View className="flex flex-row justify-between px-16 mt-6">
-        <TouchableOpacity>
-          <View
-            style={{ height: iconSize, width: iconSize }}
-            className="border border-secondary flex items-center justify-center rounded-lg"
-          >
-            <FontAwesome
-              name="download"
-              color={"#0688F3"}
-              size={ios ? 20 : 17}
-            />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <View
-            style={{ height: iconSize, width: iconSize }}
-            className="border border-secondary flex items-center justify-center rounded-lg"
-          >
-            <FontAwesome
-              name="bookmark"
-              color={"#0688F3"}
-              size={ios ? 20 : 17}
-            />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <View
-            style={{ height: iconSize, width: iconSize }}
-            className="border border-secondary flex items-center justify-center rounded-lg"
-          >
-            <FontAwesome
-              name="paper-plane"
-              color={"#0688F3"}
-              size={ios ? 20 : 17}
-            />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <View
-            style={{ height: iconSize, width: iconSize }}
-            className="border border-secondary flex items-center justify-center rounded-lg"
-          >
-            <FontAwesome
-              name="ellipsis-h"
-              color={"#0688F3"}
-              size={ios ? 20 : 17}
-            />
-          </View>
-        </TouchableOpacity>
-      </View>
-      <View className="flex flex-row mt-5 gap-5 ml-5">
-        <View className="flex flex-row gap-3 items-center">
-          <FontAwesome name="star" color={"#EF9730"} size={17} />
-          <Text className="font-lexendRegular text-tertiary text-sm">9.8</Text>
-        </View>
-        <Text className="text-white">|</Text>
-        <Text className="font-lexendSemi text-white text-sm">
-          {heure}h{minute}min
-        </Text>
-        <Text className="text-white">|</Text>
-        <Text className="font-lexendSemi text-white text-sm">
-          Action, Science Fiction
-        </Text>
-      </View>
-      <View className="mt-5 mx-5">
-        <Text className="font-lexendRegular text-justify text-white relative text-[13px] leading-6">
-          {desc}
-        </Text>
-        {long && (
-          <TouchableOpacity className="mt-1" onPress={handleMoreClick}>
-            <Text className="font-lexendRegular text-secondary text-[13px] w-24">
-              View More
+            <TouchableOpacity className="absolute  top-[75%] left-[50%] translate-x-[-50%]">
+              <View
+                style={{ height: iconSize, width: iconSize }}
+                className="rounded-full bg-secondaryTrans flex items-center justify-center"
+              >
+                <FontAwesome
+                  name="play"
+                  size={ios ? 28 : 25}
+                  color={"#0688F3"}
+                  className="ml-2"
+                />
+              </View>
+            </TouchableOpacity>
+            <View
+              className="flex flex-row justify-between px-5"
+              style={ios ? { marginTop: 60 } : { marginTop: 30 }}
+            >
+              <TouchableOpacity onPress={() => router.push("/")}>
+                <FontAwesome name="chevron-left" color={"white"} size={25} />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <FontAwesome name="tv" color={"white"} size={25} />
+              </TouchableOpacity>
+            </View>
+          </ImageBackground>
+          <View className="flex items-center justify-center px-10 w-full mt-[-25px]">
+            <Text className="text-white font-lexendSemi text-center text-xl">
+              {item.title}
             </Text>
-          </TouchableOpacity>
-        )}
-        {show && (
-          <TouchableOpacity className="mt-1" onPress={handleShowClick}>
-            <Text className="font-lexendRegular text-secondary text-[13px]">
-              Show Less
+          </View>
+          <View className="flex flex-row justify-between px-16 mt-6">
+            <TouchableOpacity>
+              <View
+                style={{ height: iconSize, width: iconSize }}
+                className="border border-secondary flex items-center justify-center rounded-lg"
+              >
+                <FontAwesome
+                  name="download"
+                  color={"#0688F3"}
+                  size={ios ? 20 : 17}
+                />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View
+                style={{ height: iconSize, width: iconSize }}
+                className="border border-secondary flex items-center justify-center rounded-lg"
+              >
+                <FontAwesome
+                  name="bookmark"
+                  color={"#0688F3"}
+                  size={ios ? 20 : 17}
+                />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View
+                style={{ height: iconSize, width: iconSize }}
+                className="border border-secondary flex items-center justify-center rounded-lg"
+              >
+                <FontAwesome
+                  name="paper-plane"
+                  color={"#0688F3"}
+                  size={ios ? 20 : 17}
+                />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View
+                style={{ height: iconSize, width: iconSize }}
+                className="border border-secondary flex items-center justify-center rounded-lg"
+              >
+                <FontAwesome
+                  name="ellipsis-h"
+                  color={"#0688F3"}
+                  size={ios ? 20 : 17}
+                />
+              </View>
+            </TouchableOpacity>
+          </View>
+          <View className="flex flex-row mt-5 gap-5 ml-5">
+            <View className="flex flex-row gap-3 items-center">
+              <FontAwesome name="star" color={"#EF9730"} size={17} />
+              <Text className="font-lexendRegular text-tertiary text-sm">
+                9.8
+              </Text>
+            </View>
+            <Text className="text-white">|</Text>
+            <Text className="font-lexendSemi text-white text-sm">
+              {heure}h{minute}min
             </Text>
-          </TouchableOpacity>
-        )}
-      </View>
-      <Cast />
-      <MovieList title="SIMILAR MOVIES" data={fruitItems} />
+            <Text className="text-white">|</Text>
+            <Text className="font-lexendSemi text-white text-sm">
+              Action, Science Fiction
+            </Text>
+          </View>
+          <View className="mt-5 mx-5">
+            <Text className="font-lexendRegular text-justify text-white relative text-[13px] leading-6">
+              {desc}
+            </Text>
+            {long && (
+              <TouchableOpacity className="mt-1" onPress={handleMoreClick}>
+                <Text className="font-lexendRegular text-secondary text-[13px] w-24">
+                  View More
+                </Text>
+              </TouchableOpacity>
+            )}
+            {show && (
+              <TouchableOpacity className="mt-1" onPress={handleShowClick}>
+                <Text className="font-lexendRegular text-secondary text-[13px]">
+                  Show Less
+                </Text>
+              </TouchableOpacity>
+            )}
+          </View>
+          <Cast />
+          {/* <MovieList title="SIMILAR MOVIES" data={fruitItems} /> */}
+        </>
+      )}
     </ScrollView>
   );
 };
--- services/tmdb.ts
+++ services/tmdb.ts
@@ -6,6 +6,7 @@ const baseUrl = "https://api.themoviedb.org/3";
 const trendingMoviesEndpoint = `${baseUrl}/trending/movie/day?api_key=${apiKey}`;
 const upcomingMoviesEndpoint = `${baseUrl}/movie/upcoming?api_key=${apiKey}`;
 const topMoviesEndpoint = `${baseUrl}/movie/top_rated?api_key=${apiKey}`;
+const findMovieByID = `${baseUrl}/movie/`;
 
 export const image500 = (path: string | null): string | null =>
   path ? `https://image.tmdb.org/t/p/w500${path}` : null;
@@ -35,3 +36,17 @@ export const fetchUpcomingMovies = (): Promise<MovieItem[]> =>
 
 export const fetchTopMovies = (): Promise<MovieItem[]> =>
   apiCall(topMoviesEndpoint);
+
+export const fetchMovieByID = async (id: number) => {
+  try {
+    const response = await fetch(`${findMovieByID}${id}?api_key=${apiKey}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data.results || [];
+  } catch (error) {
+    console.error("API call failed:", error);
+    return [];
+  }
+};
